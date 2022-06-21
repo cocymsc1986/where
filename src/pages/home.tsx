37d3877e7864569068078f35414e43cb48965fe2
Michael Cockcroft <cocymsc@hotmail.com>
2022-06-21 19:23:28 +0100

    add routes

---
+++ src/pages/home.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Form, Image } from "../components";
+
+import mockData from "../mockData.json";
+
+const HomePage = () => {
+  const correctAnswer = mockData.location;
+
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [correctGuessIndex, setCorrectGuessIndex] = useState<
+    undefined | number
+  >(undefined);
+
+  const onSubmit = (value: string) => {
+    if (value === correctAnswer) {
+      setCorrectGuessIndex(activeIndex);
+      return;
+    }
+
+    setActiveIndex((prevIndex) => prevIndex + 1);
+    console.log("submitted");
+  };
+
+  return (
+    <div>
+      <header>Where?</header>
+      <Image
+        images={mockData.images}
+        activeIndex={activeIndex}
+        correctGuessIndex={correctGuessIndex}
+      />
+      <Form
+        onSubmit={onSubmit}
+        activeIndex={activeIndex}
+        correctGuessIndex={correctGuessIndex}
+      />
+    </div>
+  );
+};
+
+export { HomePage };
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { HomePage } from "./home";
+export { UploadPage } from "./upload";
